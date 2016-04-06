f688d5a1a07a45fd6e74e15c7ce893f1c2a0101e
Matthew kingston <matthew.kingston@coles.com.au>
2016-04-06 17:44:10 +1000

    Only source new brew bash completion for bash version 4.3

--- bash/osx/autocomplete.sh
+++ bash/osx/autocomplete.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-if [[ -f "$(brew --prefix)"/share/bash-completion/bash_completion ]]; then
-  # Use `bash-completion` 2.x
-  source "$(brew --prefix)"/share/bash-completion/bash_completion
+if [[ "$(bash --version)" =~ "version 4.3" ]]; then
+  if [[ -f "$(brew --prefix)"/share/bash-completion/bash_completion ]]; then
+    # Use `bash-completion` 2.x
+    source "$(brew --prefix)"/share/bash-completion/bash_completion
 
-  # Make bash complete the `g` alias, just like it does `git`
-  complete -o default -o nospace -F _git g
+    # Make bash complete the `g` alias, just like it does `git`
+    complete -o default -o nospace -F _git g
+  fi
 fi
 
 # AWS Cli completion
